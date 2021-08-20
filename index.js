aaadd7dcb3180d6f2b6b67ce01d937746df4afa2
MonicaGonzalez30 <monik196901@gmail.com>
2021-08-19 19:52:08 -0500

    Se crean funciones asincronas

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function name(){
-    
-}
+// function llamada(){
+//     console.log("Hola mundo");
+// }
+
+// function llamadora(call){
+//     console.log("Dentro de esta funcion llamamos nuestro callback")
+// }
+
+// llamdadora(llamada);
+setTimeout(() => {
+    console.log("1 Esto va primero");
+}, 6000)
+console.log("2 Esto va segundo");
+setTimeout(() => {
+    console.log("3 Esto va tercero");
+}, 4000)
+console.log("4 Esto va cuarto");
